8d4eea9a9cce2dc2a518b917e817fafc491d9a05
hongju1557 <fuyuka666@gmail.com>
2026-07-24 11:12:22 +0900

    Add java practice files

--- src/App.java
+++ src/App.java
@@ -1,7 +1,19 @@
 
-
+//클래스 선언부
+//클래스명 = 파일의 이름
+//첫글자는 대문자로 작성
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+
+    
+    /**
+     * 프로그램 주석
+     * @param args
+    */
+   // main 메서드 : 프로그램 실행
+   //메서드이름(매개변수)
+   //접근제한자 반환타입 메서드이름(매개변수타입 매개변수이름)
+   public static void main(String[] args) {
+    // 자바의 표준 출력
+    System.out.println("Hello, World!");
+}
 }
---
+++ src/MyApp.java
@@ -0,0 +1,13 @@
+//클래스선언부
+// 코드블록 {}
+// 접근제한자 class 클래스명 {}
+
+public class MyApp {
+
+    //속성 = 필드
+    
+    //기능 = 메서드
+    public static void main(String[] args){
+        System.out.println("테스트");
+    }
+}
